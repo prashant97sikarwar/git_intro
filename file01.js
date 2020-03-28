f316c82999aba5ec36cc045ddbe8709585f29464
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-03-28 22:53:58 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,5 +1,7 @@
 function greet(name){
-    console.log('Hello ' + name);
+    console.log('knock knock knock' + name);
+    console.log('knock knock knock' + name);
+    console.log('knock knock knock' + name);
     
 }
 greet('prashant')
--- file02.js
+++ file02.js
@@ -1,1 +1,4 @@
 console.log("hey! we're learning git");
+console.log("use 'git status' to check status");
+console.log("use 'git log' to see history");
+
